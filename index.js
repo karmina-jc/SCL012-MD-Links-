973c7afdc56777b185f98ab39a577a382fcc3eec
karmina-jc <karmina.jarac@gmail.com>
2020-02-19 21:31:31 -0300

    actualizacion funcionalidad mdlinks

--- index.js
+++ index.js
@@ -1,54 +1,62 @@
 const fs = require('fs');
 const path = require('path');
 const regExp = /\]\((http[^)]+)\)/g
-const fetchUrl = require("fetch").fetchUrl;;
+const fetch = require('node-fetch')
+
+
+// promesa lee carpeta
+const throughFolder = (location) => {
+  return new Promise ((resolve, reject) => {
+    fs.readdir(location, (err, files) => {
+      if (err) {
+      reject(err)
+      }else{
+      resolve(files)       
+      }
+    })                   
+  })
+}
+
+// promesa que lee archivo y toma links
+const takeLinks = (docToRead) => {
+  return new Promise ((resolve, reject) => {
+    fs.readFile(docToRead, 'utf8', (err, data) => {
+      if (err){
+      reject(err)
+      }else{
+        let cleanLinks = []
+        const allLinks = data.match(regExp)
+        allLinks.forEach(e => {
+          cleanLinks.push(e.replace(/[\[\(\)\]]/g, ''))
+        })
+      resolve(cleanLinks)
+      }        
+    })
+  })
+}
 
 module.exports = {
-  // Funcion para leer archivo
-  throughFolder: (location) => {
-    return new Promise ((resolve, reject) => {
-      fs.readdir(location, (err, files) => {
-        if (err) {
-        reject(err)
-        }else{
-        resolve(files)       
-        }
-      })                   
-    })
-  },
-  takeLinks: (docToRead) => {
-    return new Promise ((resolve, reject) => {
-      fs.readFile(docToRead, 'utf8', (err, data) => {
-        if (err){
-        reject(err)
-        }else{
-          let cleanLinks = []
-          const allLinks = data.match(regExp)
-          allLinks.forEach(e => {
-            cleanLinks.push(e.replace(/[\[\(\)\]]/g, ''))
+  mdLinks: (location) => {
+    return new Promise((resolve, reject) => {
+      if(path.extname(location) === '.md'){
+        takeLinks(location)
+        .then (links => {
+          let checkedLinks = []
+          links.map(url => {
+            fetch(url)
+            .then(res => {
+              checkedLinks.push(location, url.split(0, 51), res.status)
+            })              
+            })                     
+            resolve(checkedLinks)
           })
-        resolve(cleanLinks)
-        }        
-      })
-    })
-  },
-  urlStatus: (url) => {
-    return new Promise ((resolve, reject) => {
-     fetchUrl(url, (error, meta, body) => {
-       if (error) {
-         reject (error)
-       } else {
-         resolve (meta)
-       }
-      })
-    })
+      }else{
+        reject('Ingresa ruta de archivo Markdown')
+      }
+    })  
   }
-
 };
 
-/*let selectFiles = []
-          files.forEach(e => {
-            if(path.extname(e) === '.md'){
-              selectFiles.push(e)  
-            }    
-          })*/
+mdLinks('./linksprueba.md')
+
+const comand = process.argv[2]
